47b6324e2bccce346a706fa3b5cbcfbbdfbca68b
StevenMODIMO <stevepetes26@gmail.com>
2023-11-24 09:19:37 +0300

    typescript

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+// arrays
+let names = ['Steven', 'Peter', 'Modimo'];
+let ages = [1, 2, 3, 4, 45, 6, 77];
+names.pop();
+names.push('Biko'); // place an item at the end od the array
+names.shift(); // remove the first item from an array
+names.unshift('Huncho'); // adds item at the begining of the array
+// type inference with arrays
+let con = ['rain', 'snow', 'sleet', 'hail', 'clear'];
+const cons = con.slice(1, 3);
+console.log(cons);
+// object literals
+// type inference with object literals
